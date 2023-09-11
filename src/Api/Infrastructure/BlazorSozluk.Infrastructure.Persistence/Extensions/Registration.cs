2b04ed07b8172c7b062b326937d0e78cc66c0c3d
buraksahbaz71 <buraksahbaz@mlgbilisim.com.tr>
2023-09-11 23:40:03 +0300

    Add project files.

---
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/Extensions/Registration.cs
@@ -0,0 +1,34 @@
+﻿using BlazorSozluk.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Infrastructure.Persistence.Extensions
+{
+    public static class Registration
+    {
+        public static IServiceCollection AddInfrastructureRegistration(this IServiceCollection services, IConfiguration configuration)
+        {
+            
+            services.AddDbContext<BlazorSozlukContext>(conf =>
+            {
+                string connStr = configuration["BlazorSozlukDbConnectionString"].ToString();
+                conf.UseSqlServer(connStr, opt =>
+                {
+                    opt.EnableRetryOnFailure();
+                });
+            });
+
+            /* Her seferin yeni veriler üretmesin diye yorum satırına alındı! */
+            //var seedData = new SeedData();
+            //seedData.SeedAsycn(configuration).GetAwaiter().GetResult();
+
+            return services;
+        }
+    }
+}
