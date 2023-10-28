f2960a1fcbc4a83d98167ae1a0aac7a9eee4545a
buraksahbaz71 <buraksahbaz@mlgbilisim.com.tr>
2023-10-28 23:00:02 +0300

    CSS and EntriesComponent added

--- src/Api/Core/BlazorSozluk.Api.Application/Features/Queries/SearchBySubject/SearchEntryQueryHandler.cs
+++ src/Api/Core/BlazorSozluk.Api.Application/Features/Queries/SearchBySubject/SearchEntryQueryHandler.cs
@@ -22,7 +22,7 @@ namespace BlazorSozluk.Api.Application.Features.Queries.SearchBySubject
         public async Task<List<SearchEntryViewModel>> Handle(SearchEntryQuery request, CancellationToken cancellationToken)
         {
             // TODO validation, request.serachtext length should be checked
-            var result = entryRepository.Get(i => EF.Functions.Like(i.Subject, $"{request.SearchText}")).Select(i => new SearchEntryViewModel
+            var result = entryRepository.Get(i => EF.Functions.Like(i.Subject, $"%{request.SearchText}%")).Select(i => new SearchEntryViewModel
             {
                 Id = i.Id,
                 Subject = i.Subject
